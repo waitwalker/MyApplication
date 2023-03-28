c58131b3f7c91170a33ea8cc51dc29d8d46496af
liuchuanan <liuchuanan@xianlai-inc.com>
2023-03-28 14:19:58 +0800

    Initial commit

---
+++ counter/src/main/java/com/example/counter/Counter.java
@@ -0,0 +1,13 @@
+package com.example.counter;
+
+public class Counter {
+
+    int add(int x, int y) {
+        return x + y;
+    }
+
+    int subtract(int x, int y) {
+        return add(x,-y);
+    }
+
+}
